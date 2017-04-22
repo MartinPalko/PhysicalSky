24b3bcf7691fd9486a162ed1aded0cab878b9ab5
Martin Palko <jeeprubi@hotmail.com>
2017-04-21 20:30:47 -0400

    Created small library for working with various forms of celestial coordinates.

---
+++ Scripts/Editor/Tests/CelestialCoordinatesTests.cs
@@ -0,0 +1,96 @@
+﻿#if PHYSICAL_SKY_TESTS
+using UnityEngine;
+
+namespace PhysicalSky
+{
+    using NUnit.Framework;
+    using CelestialCoordinates;
+
+    [TestFixture]
+    public class CelestialCoordinatesTests
+    {
+        static float roundFloat(float source, int decimalPoints)
+        {
+            float mult = Mathf.Pow(10, decimalPoints);
+            return Mathf.Round((source * mult)) / mult;
+        }
+
+        [Test]
+        public void CartesianToSpherical()
+        {
+            CartesianCoords sourceCoords = new CartesianCoords(3.0f, 4.0f, 5.0f);
+            SphericalCoords castCoords = sourceCoords;
+            SphericalCoords answer = new SphericalCoords(0.78539816339745f, 0.92729521800161f, 7.0710678118655f);
+            Assert.AreEqual(answer.ToString(), castCoords.ToString()); // Silly, but compare string versions, because they have rounded floats the mitigate floating point precision inaccuracies.
+        }
+
+        [Test]
+        public void SphericalToCartesian()
+        {
+            SphericalCoords sourceCoords = new SphericalCoords(1.236f, 0.35f, 5.0f);
+            CartesianCoords castCoords = sourceCoords;
+            CartesianCoords answer = new CartesianCoords(4.43608079f, 1.619295894f, 1.64288406f);
+            Assert.AreEqual(answer.ToString(), castCoords.ToString());
+        }
+
+        [Test]
+        public void EquitorialToCartesian()
+        {
+            EquitorialCoordinates sourceCoords = new EquitorialCoordinates(14.4966f, -62.681f, 1.29f);
+            CartesianCoords castCoords = sourceCoords;
+
+            // Round, because our test case result has limited precision.
+            castCoords.x = roundFloat(castCoords.x, 3);
+            castCoords.y = roundFloat(castCoords.y, 3);
+            castCoords.z = roundFloat(castCoords.z, 3);
+
+            CartesianCoords answer = new CartesianCoords(-0.470f, -0.360f, -1.146f);
+            Assert.AreEqual(answer, castCoords);
+        }
+
+        [Test]
+        public void GalacticToJ2000()
+        {
+            // Test case created from: http://www.robertmartinayers.org/tools/coordinates.html
+
+            GeographicCoords testCoord = new GeographicCoords(359.944f * Mathf.Deg2Rad, -0.0461f * Mathf.Deg2Rad);
+            EquitorialCoordinates converted = Utility.GalacticToJ2000(testCoord);
+            converted.ra = roundFloat(converted.ra, 3);
+            converted.dec = roundFloat(converted.dec, 3);
+            converted.d = roundFloat(converted.d, 3);
+
+            EquitorialCoordinates answer = new EquitorialCoordinates((266.417f / 360.0f) * 24.0f, -29.008f);
+            answer.ra = roundFloat(answer.ra, 3);
+            answer.dec = roundFloat(answer.dec, 3);
+            answer.d = roundFloat(answer.d, 3);
+
+            Assert.AreEqual(answer, converted);
+        }
+
+        [Test]
+        public void EquitorialCoordinates()
+        {
+            int testRAHours = 12;
+            int testRAMinutes = 23;
+            float testRASeconds = 26.20f;
+            int testDecDegrees = 84;
+            int testDecArcMinutes = 31;
+            float testDecArcSeconds = 58.64f;
+
+            // Test converting to/from Hours/minutes/seconds and degrees/arcmin/arcsec.
+            EquitorialCoordinates testCoordinate = new EquitorialCoordinates(testRAHours, testRAMinutes, testRASeconds, testDecDegrees, testDecArcMinutes, testDecArcSeconds);
+            testRASeconds = roundFloat(testRASeconds, 0);
+            testDecArcSeconds = roundFloat(testDecArcSeconds, 0);
+            float answerRASeconds = roundFloat(testCoordinate.RASeconds, 0);
+            float answerDecArcSeconds = roundFloat(testCoordinate.DecArcSeconds, 0);
+            Assert.AreEqual(testRAHours, testCoordinate.RAHours, "Ra Hours");
+            Assert.AreEqual(testRAMinutes, testCoordinate.RAMinutes, "Ra Minutes");
+            Assert.AreEqual(testRASeconds, answerRASeconds, "Ra Seconds");
+            Assert.AreEqual(testDecDegrees, testCoordinate.DecDegrees, "Dec Degrees");
+            Assert.AreEqual(testDecArcMinutes, testCoordinate.DecArcMin, "Dec Arc Minutes");
+            Assert.AreEqual(testDecArcSeconds, answerDecArcSeconds, "Dec Arc Seconds");
+        }
+
+    }
+}
+#endif
---
+++ Scripts/Utility/CelestialCoordinates.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+
+namespace PhysicalSky
+{
+    namespace CelestialCoordinates
+    {
+        [System.Serializable]
+        public struct CartesianCoords
+        {
+            public float x;
+            public float y;
+            public float z;
+
+            public CartesianCoords(float x, float y, float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public override string ToString()
+            {
+                return "(x" + x + ", y" + y + ", z" + z + ")";
+            }
+
+            public static implicit operator CartesianCoords(Vector3 v) { return new CartesianCoords(v.x, v.z, v.y); }
+            public static implicit operator CartesianCoords(SphericalCoords s) { return Utility.SphericalToCartesian(s); }
+            public static implicit operator CartesianCoords(GeographicCoords g) { return Utility.SphericalToCartesian(g); }
+            public static implicit operator CartesianCoords(EquitorialCoordinates e) { return Utility.EquitorialToCartesian(e); }
+
+            public Vector3 ToVector3()
+            {
+                // Cartesian Coords are Z-up, whereas unity is Y-up
+                return new Vector3(x, z, y);
+            }
+        }
+
+        [System.Serializable]
+        public struct SphericalCoords
+        {
+            // Zenith and Azimuth in radians
+            public float zenith;
+            public float azimuth;
+            public float radius;
+
+            public SphericalCoords(float zenith, float azimuth, float radius = 1.0f)
+            {
+                this.radius = radius;
+                this.zenith = zenith;
+                this.azimuth = azimuth;
+            }
+
+            public override string ToString()
+            {
+                return "(zenith" + zenith + ", azimuth" + azimuth + ", radius" + radius + ")";
+            }
+
+            public static implicit operator SphericalCoords(CartesianCoords c) { return Utility.CartesianToSpherical(c); }
+            public static implicit operator SphericalCoords(GeographicCoords g) { return new SphericalCoords((Mathf.Deg2Rad * 90.0f) - g.latitude, g.longitude); }
+        }
+
+        [System.Serializable]
+        public struct GeographicCoords
+        {
+            // Latitude and Longitude in radians
+            public float latitude;
+            public float longitude;
+
+            public GeographicCoords(float latitude, float longitude)
+            {
+                this.latitude = latitude;
+                this.longitude = longitude;
+            }
+
+            public override string ToString()
+            {
+                return "(latitude" + latitude + ", longitude" + longitude + ")";
+            }
+
+            public static implicit operator GeographicCoords(SphericalCoords s) { return new GeographicCoords(s.zenith + (Mathf.Deg2Rad * 90.0f), s.azimuth); }
+        }
+
+        [System.Serializable]
+        public struct EquitorialCoordinates
+        {
+            public float ra; // Right Ascension stored in hours, properties provided for minutes, and seconds.            
+            public float dec; // Declination stored in degrees, properties provided for arc minutes, and arc seconds.
+            public float d; // Distance
+
+            public EquitorialCoordinates(float ra, float dec, float d = 1.0f)
+            {
+                this.ra = ra;
+                this.dec = dec;
+                this.d = d;
+            }
+
+            public EquitorialCoordinates(int raHours, int raMinutes, float raSeconds, int decDegrees, int decArcMinutes, float decArcSeconds, float distance = 1.0f)
+            {
+                ra = 0.0f;
+                dec = 0.0f;
+                d = distance;
+
+                SetRA(raHours, raMinutes, raSeconds);
+                SetDec(decDegrees, decArcMinutes, decArcSeconds);
+            }
+
+            public override string ToString()
+            {
+                return "(ra" + ra + ", dec" + dec + ", d" + d + ")";
+            }
+
+            public int RAHours { get { return (int)(ra); } }
+            public int RAMinutes { get { return (int)((ra * 60.0f) % 60.0f); } }
+            public float RASeconds { get { return (float)((ra * 60.0f * 60.0f) % 60.0f); } }
+
+            public int DecDegrees { get { return (int)(dec); } }
+            public int DecArcMin { get { return (int)((dec * 60.0f) % 60.0f); } }
+            public float DecArcSeconds { get { return (float)((dec * 60.0f * 60.0f) % 60.0f); } }
+
+            public void SetRA(int hours, int minutes, float seconds)
+            {
+                ra = (hours + minutes / 60.0f + seconds / (60.0f * 60.0f));
+            }
+
+            public void SetDec(int degrees, int arcMinutes, float arcSeconds)
+            {
+                dec = (degrees + arcMinutes / 60.0f + arcSeconds / (60.0f * 60.0f));
+            }
+        }
+
+        public static class Utility
+        {
+            public static SphericalCoords CartesianToSpherical(CartesianCoords cartesian)
+            {
+                float radius = Mathf.Sqrt(cartesian.x * cartesian.x + cartesian.y * cartesian.y + cartesian.z * cartesian.z);
+                float azimuth = Mathf.Atan2(cartesian.y, cartesian.x);
+                float zenith = Mathf.Acos(cartesian.z / radius);
+
+                return new SphericalCoords(zenith, azimuth, radius);
+            }
+
+            public static CartesianCoords SphericalToCartesian(SphericalCoords spherical)
+            {
+                float sinZenith = Mathf.Sin(spherical.zenith);
+
+                float x = spherical.radius * sinZenith * Mathf.Cos(spherical.azimuth);
+                float y = spherical.radius * sinZenith * Mathf.Sin(spherical.azimuth);
+                float z = spherical.radius * Mathf.Cos(spherical.zenith);
+
+                return new CartesianCoords(x, y, z);
+            }
+
+            public static CartesianCoords EquitorialToCartesian(EquitorialCoordinates equitorial)
+            {
+                float ra = (equitorial.ra * 15.0f * Mathf.PI) / 180.0f;
+                float dec = (equitorial.dec * Mathf.PI) / 180.0f;
+                float d = equitorial.d;
+                float cosDec = Mathf.Cos(dec);
+
+                float x = d * cosDec * Mathf.Cos(ra);
+                float y = d * cosDec * Mathf.Sin(ra);
+                float z = d * Mathf.Sin(dec);
+
+                return new CartesianCoords((float)x, (float)y, (float)z);
+            }
+
+            public static EquitorialCoordinates GalacticToJ2000(GeographicCoords galactic)
+            {
+                //https://gist.github.com/barentsen/2367839#file-ga2equ-py-L15
+                float l = galactic.latitude;
+                float b = galactic.longitude;
+
+                // North galactic pole (J2000)
+                float pole_ra = 192.859508f * Mathf.Deg2Rad;
+                float pole_dec = 27.128336f * Mathf.Deg2Rad;
+                float posangle = (122.932f - 90.0f) * Mathf.Deg2Rad;
+
+                float ra = Mathf.Atan2((Mathf.Cos(b) * Mathf.Cos(l - posangle)), (Mathf.Sin(b) * Mathf.Cos(pole_dec) - Mathf.Cos(b) * Mathf.Sin(pole_dec) * Mathf.Sin(l - posangle))) + pole_ra;
+                float dec = Mathf.Asin(Mathf.Cos(b) * Mathf.Cos(pole_dec) * Mathf.Sin(l - posangle) + Mathf.Sin(b) * Mathf.Sin(pole_dec));
+
+                return new EquitorialCoordinates((ra * Mathf.Rad2Deg / 360.0f) * 24.0f, dec * Mathf.Rad2Deg);
+
+                //Vector3 xAxis = ((CartesianCoords)new EquitorialCoordinates(21, 12, 1.1f, 48, 19, 47.0f)).ToVector3(); // Galactic Long 90, Lat 0 in J2000
+                //Vector3 yAxis = ((CartesianCoords)new EquitorialCoordinates(17, 45, 37.2f, -28, 56, 10.0f)).ToVector3(); ; // Galactic Long 0, Lat 0 in J2000
+                //Vector3 zAxis = ((CartesianCoords)new EquitorialCoordinates(12, 51, 26.3f, 27, 7, 42.0f)).ToVector3(); ; // Galactic Long 0, Lat 90 in J2000
+
+
+                //Vector3 c = galactic.ToVector3();
+
+                //Vector3 result;
+                //result.x = Vector3.Dot(xAxis, c);
+                //result.y = Vector3.Dot(yAxis, c);
+                //result.z = Vector3.Dot(zAxis, c);
+
+                //return result;
+            }
+        }
+    }
+}
