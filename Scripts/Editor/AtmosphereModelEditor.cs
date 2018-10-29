21d53c86bacdf1fd1638d040623b15e44e473fee
Martin Palko <jeeprubi@hotmail.com>
2018-10-29 15:27:00 -0400

    Added custom editor for AtmosphereModel

---
+++ Scripts/Editor/AtmosphereModelEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PhysicalSky
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(AtmosphereModel))]
+    public class AtmosphereModelEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Compute Atmosphere"))
+            {
+                foreach (var t in targets)
+                {
+                    var atmosphereModel = t as AtmosphereModel;
+                    atmosphereModel.Compute(true);
+                }
+            }
+
+        }
+    }
+}
