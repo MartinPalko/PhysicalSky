dce425c5e8a4529f9f0a4e4c259baa9b4c933581
Martin Palko <jeeprubi@hotmail.com>
2018-10-30 16:50:12 -0400

    Allow serialization of lookup textures so they can be loaded from disk instead of recomputed each time.

--- Scripts/Atmosphere/AtmosphereModel.cs
+++ Scripts/Atmosphere/AtmosphereModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using System;
 
 using PhysicalSky.Utilities;
@@ -37,16 +40,100 @@ namespace PhysicalSky
         }
 
         // Computed values
+        [SerializeField] [HideInInspector]
         private AtmosphereParameters m_computedParameters;
         public AtmosphereParameters ComputedParameters
         {
             get { return m_computedParameters; }
         }
+        [SerializeField] [HideInInspector]
         private ComputedRuntimeValues m_computedValues;
-        // Computed textures
+
+        // Temporary lookup textures output from the computation
         private RenderTexture m_transmittanceLUT = null;
         private RenderTexture m_scatteringLUT = null;
         private RenderTexture m_irradianceLUT = null;
+
+        private enum SerializeTextureMethod
+        {
+            SubAsset = 0,
+            SeperateAsset = 1,
+            DontSerialize = 2,
+        }
+
+        [SerializeField]
+        private SerializeTextureMethod m_serializeTextures = SerializeTextureMethod.SubAsset;
+
+        [SerializeField] [HideInInspector]
+        private Texture2D m_savedTransmittanceLUT = null;
+        [SerializeField] [HideInInspector]
+        private Texture2D m_savedScatteringLUT = null;
+        [SerializeField] [HideInInspector]
+        private Texture2D m_savedIrradianceLUT = null;
+
+#if UNITY_EDITOR
+        private void DeleteTexture(ref Texture2D texture)
+        {
+            if (m_savedTransmittanceLUT)
+            {
+                string transmittanceAssetPath = AssetDatabase.GetAssetPath(m_savedTransmittanceLUT);
+
+                if (transmittanceAssetPath != AssetDatabase.GetAssetPath(this))
+                    AssetDatabase.DeleteAsset(transmittanceAssetPath);
+
+                DestroyImmediate(m_savedTransmittanceLUT, true);
+                m_savedTransmittanceLUT = null;
+            }
+        }
+
+        private Texture2D SaveTexture(RenderTexture texture, string name)
+        {
+            Texture2D savedTexture = GraphicsHelpers.Texture2DFromRenderTexture(texture);
+            savedTexture.name = name;
+
+            string atmosphereAssetPath = AssetDatabase.GetAssetPath(this);
+            string atmosphereAssetPathNoExtension = System.IO.Path.ChangeExtension(atmosphereAssetPath, null);
+
+            if (m_serializeTextures == SerializeTextureMethod.SubAsset)
+            {
+                AssetDatabase.AddObjectToAsset(savedTexture, atmosphereAssetPath);
+            }
+            else if (m_serializeTextures == SerializeTextureMethod.SeperateAsset)
+            {
+                AssetDatabase.CreateAsset(savedTexture, atmosphereAssetPathNoExtension + "." + name + ".asset");
+            }
+            else
+            {
+                throw new System.NotImplementedException();
+            }
+
+            return savedTexture;
+        }
+
+        private void UpdateSavedTextures()
+        {
+            DeleteTexture(ref m_savedTransmittanceLUT);
+            DeleteTexture(ref m_savedScatteringLUT);
+            DeleteTexture(ref m_savedIrradianceLUT);
+
+            // Clean up just incase any sub-assets were left over
+            foreach (UnityEngine.Object subAsset in AssetDatabase.LoadAllAssetsAtPath(AssetDatabase.GetAssetPath(this)))
+            {
+                if (subAsset != this)
+                    DestroyImmediate(subAsset, true);
+            }
+
+            if (m_serializeTextures != SerializeTextureMethod.DontSerialize)
+            {
+                m_savedTransmittanceLUT = SaveTexture(m_transmittanceLUT, "TransmittanceLUT");
+                m_savedScatteringLUT = SaveTexture(m_scatteringLUT, "ScatteringLUT");
+                m_savedIrradianceLUT = SaveTexture(m_irradianceLUT, "IrradianceLUT");
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+#endif
 
         public bool HasComputedData()
         {
@@ -56,15 +143,14 @@ namespace PhysicalSky
         public bool NeedsRecompute()
         {
             return (m_computedParameters != m_parameters) || !HasComputedData();
-
         }
 
         private void SetShaderUniforms(GraphicsHelpers.IMaterialProperties m)
         {
             // TODO: Set only run-time required values here
-            m.SetTexture("_transmittance_texture", m_transmittanceLUT);
-            m.SetTexture("_scattering_texture", m_scatteringLUT);
-            m.SetTexture("_irradiance_texture", m_irradianceLUT);
+            m.SetTexture("_transmittance_texture", m_transmittanceLUT != null ? (Texture)m_transmittanceLUT : m_savedTransmittanceLUT);
+            m.SetTexture("_scattering_texture", m_scatteringLUT != null ? (Texture)m_scatteringLUT : m_savedScatteringLUT);
+            m.SetTexture("_irradiance_texture", m_irradianceLUT != null ? (Texture)m_irradianceLUT : m_savedIrradianceLUT);
             m.SetVector("_sky_spectral_radiance_to_luminance", m_computedParameters.luminance != AtmosphereParameters.LuminanceType.none ? m_computedValues.m_sky_k : Vector3.one);
             m.SetVector("_sun_spectral_radiance_to_luminance", m_computedParameters.luminance != AtmosphereParameters.LuminanceType.none ? m_computedValues.m_sun_k : Vector3.one);
             m.SetVector("_solar_irradiance", m_computedValues.m_solarIrradiance);
@@ -94,7 +180,7 @@ namespace PhysicalSky
         {
             if (NeedsRecompute() || force)
             {
-                AllocateLookupTextures();
+                AllocateRenderTextures();
 
                 Prerenderer preRenderer;
 
@@ -111,6 +197,16 @@ namespace PhysicalSky
                 if (preRenderer.Compute(m_parameters, m_transmittanceLUT, m_scatteringLUT, m_irradianceLUT, ref m_computedValues))
                 {
                     m_computedParameters = m_parameters;
+#if UNITY_EDITOR
+                    UpdateSavedTextures();
+
+                    if (m_serializeTextures != SerializeTextureMethod.DontSerialize)
+                    {
+                        ReleaseRenderTextures();
+                    }
+
+                    EditorUtility.SetDirty(this);
+#endif
                     return true;
                 }
                 else
@@ -126,18 +222,18 @@ namespace PhysicalSky
 #if PHYSICAL_SKY_DEBUG
             Debug.Log("Released Atmosphere Resources");
 #endif
-            ReleaseLookupTextures();
+            ReleaseRenderTextures();
             m_computedParameters = new AtmosphereParameters();
         }
 
-        private void AllocateLookupTextures()
+        private void AllocateRenderTextures()
         {
             TextureFactory.CreateRenderTexture(ref m_transmittanceLUT, TextureFactory.Preset.Transmittance);
             TextureFactory.CreateRenderTexture(ref m_irradianceLUT, TextureFactory.Preset.Irradiance);
             TextureFactory.CreateRenderTexture(ref m_scatteringLUT, TextureFactory.Preset.Scattering);
         }
 
-        private void ReleaseLookupTextures()
+        private void ReleaseRenderTextures()
         {
             TextureFactory.ReleaseRenderTexture(ref m_transmittanceLUT);
             TextureFactory.ReleaseRenderTexture(ref m_irradianceLUT);
@@ -146,12 +242,11 @@ namespace PhysicalSky
 
         public bool TexturesInvalid()
         {
-            if (!m_transmittanceLUT || !m_transmittanceLUT.IsCreated())
+            if (m_savedIrradianceLUT == null && (m_transmittanceLUT == null || !m_transmittanceLUT.IsCreated()))
                 return true;
-            else if (!m_scatteringLUT || !m_scatteringLUT.IsCreated())
+            else if (m_savedScatteringLUT == null && (m_scatteringLUT == null || !m_scatteringLUT.IsCreated()))
                 return true;
-
-            else if (!m_irradianceLUT || !m_irradianceLUT.IsCreated())
+            else if (m_savedIrradianceLUT == null && (m_irradianceLUT == null || !m_irradianceLUT.IsCreated()))
                 return true;
             else
                 return false;
