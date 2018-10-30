b2371a171f56e2b055273edb57d2f2aab66e5181
Martin Palko <jeeprubi@hotmail.com>
2018-10-30 15:28:10 -0400

    Added physical sky to shader includes

---
+++ Scripts/Editor/ShaderIncludes.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace PhysicalSky
+{
+    static class ShaderIncludes
+    {
+#if UNITY_2018_1_OR_NEWER
+        [ShaderIncludePath]
+#endif
+        public static string[] GetPaths()
+        {
+            return new string[] {
+                Path.Combine(Application.dataPath, "PhysicalSky/Shaders/"),
+                };
+        }
+    }
+}
